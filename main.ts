91afca71c01a89a89cc33141c6cd21a4c547fbb1
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2023-12-13 10:44:50 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Skull)
-    music.playMelody("E C E A B A B A ", 300)
+    music.play(music.stringPlayable("E D E A B A B A ", 300), music.PlaybackMode.LoopingInBackground)
 })
